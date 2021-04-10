637d6651ebdcd87600b46049a8d855bfd0883d73
Tae Prasongpongchai <taepras@gmail.com>
2021-04-11 01:30:13 +0700

    more styling and layout

--- src/components/D3Component.jsx
+++ src/components/D3Component.jsx
@@ -1,19 +1,88 @@
-import React, { useRef, useEffect, useMemo, useCallback, useState } from 'react';
+import React, { useRef, useEffect, useLayoutEffect, useMemo, useCallback, useState } from 'react';
 import * as d3 from 'd3';
 // import { transition } from 'd3';
+import useDimensions from "react-cool-dimensions";
+import styled from 'styled-components';
+import { useResizeDetector } from 'react-resize-detector';
 
-const transitionDuration = 1000
+
+const SvgContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+`
+
+const Fade = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 20%;
+    background: linear-gradient(to bottom, #222, #2220);
+`
+
+const CartesianSvg = styled.svg`
+    & > g text {
+        transform: scale(1, -1);
+    }
+`
+
+// const transitionDuration = 1000
 
 export const D3Component = ({
     income,
     expense,
     allowance,
     taxBrackets,
-    width = 800,
-    height = 500,
-    padding = 50,
+    // width = 800,
+    // height = 500,
+    padding = {
+        left: 35,
+        right: 0,
+        top: 0,
+        bottom: 50,
+    },
+    isPullTax = false,
+    transitionTime = 500,
+    enableTransition = true
 }) => {
+    const { observe, unobserve, width, height, entry } = useDimensions({
+        onResize: ({ observe, unobserve, width, height, entry }) => {
+            // Triggered whenever the size of the target is changed...
+
+            unobserve(); // To stop observing the current target element
+            observe(); // To re-start observing the current target element
+        },
+    });
+
+    const transitionDuration = useMemo(() => enableTransition ? transitionTime : 0, [enableTransition, transitionTime]);
+
+    // const { width, height, chartContainer } = useResizeDetector();
+
     const d3Container = useRef(null);
+
+    // const { observe, unobserve, width, height, entry } = useDimensions({
+    //     onResize: ({ observe, unobserve, width, height, entry }) => {
+    //         // Triggered whenever the size of the target is changed...
+
+    //         unobserve(); // To stofp observing the current target element
+    //         observe(); // To re-start observing the current target element
+    //     },
+    // });
+
+    // const height = d3Container.current.clientHeight;
+    // const width = d3Container.current.clientWidth
+    // const [width, setWidth] = useState(0)
+    // const [height, setHeight] = useState(0)
+
+    // useLayoutEffect(() => {
+    //     if (d3Container.current) {
+    //         setWidth(d3Container.current.parentElement.offsetWidth);
+    //         setHeight(d3Container.current.parentElement.offsetHeight);
+    //     }
+    // }, [d3Container])
 
     const cleanedExpense = useMemo(() => expense < (income - allowance) ? +expense : +Math.max(0, income - allowance), [income, expense]);
     const cleanedAllowance = useMemo(() => allowance < income ? +allowance : +income, [income, allowance]);
@@ -24,55 +93,77 @@ export const D3Component = ({
 
     const scaleIncome = useCallback(
         d3.scaleLinear()
-            .domain([0, Math.max(300_000, income * 1.5)])
-            .range([0, width])
-        , [income])
+            .domain([0, Math.max(300_000, income * 1.4)])
+            .range([0, height - padding.bottom])
+        , [height, income, padding])
 
-    const barHeight = useMemo(() => height - 50 - padding * 2, [height, padding]);
+    const barWidth = useMemo(() => width - (padding.left + padding.right), [width, padding]);
 
     const scaleTaxRate = useCallback(
         d3.scaleLinear()
-            .domain([1, 0])
-            .range([0, barHeight])
-        , [income])
+            .domain([0, 1])
+            .range([0, barWidth])
+        , [income, width])
 
-    const axisIncomeBottom = useMemo(() => d3.axisBottom(scaleIncome), [scaleIncome]);
-    const axisIncomeTop = useMemo(() => d3.axisTop(scaleIncome), [scaleIncome]);
-    const axisTaxRate = useMemo(() => d3.axisLeft(scaleTaxRate).tickFormat(d3.format('.0%'))
+    const axisIncomeLeft = useMemo(() => d3.axisLeft(scaleIncome)
+        .tickFormat(d3.format(".3s"))
+        , [scaleIncome]);
+    const axisIncomeRight = useMemo(() => d3.axisRight(scaleIncome), [scaleIncome]);
+    const axisTaxRate = useMemo(() => d3.axisBottom(scaleTaxRate).tickFormat(d3.format('.0%'))
         , [scaleTaxRate]);
-
-    const [isPullTax, setPullTax] = useState(false);
 
     const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (d3Container.current) {
+
+            // console.log(d3Container.current);
+
+
+            // const boundingRect = d3Container.current.getClientBoundingRect();
 
             const svg = d3.select(d3Container.current);
 
             svg.select('g.container')
-                .attr('transform', `translate(${padding}, ${padding})`);
+                .attr('transform', `translate(${padding.left}, ${padding.bottom})`);
 
             svg.select('rect.net-income')
-                .attr('height', barHeight)
+                .attr('width', barWidth)
                 .transition().duration(transitionDuration)
                 .style('fill', '#08f')
-                .attr('width', Math.max(0, scaleIncome(netIncome)));
+                .attr('height', Math.max(0, scaleIncome(netIncome)));
 
             console.log('netIncome + cleanedAllowance', netIncome + cleanedAllowance, cleanedAllowance, scaleIncome(cleanedAllowance))
             svg.select('rect.expense')
-                .attr('height', barHeight)
+                .attr('width', barWidth)
                 .transition().duration(transitionDuration)
                 .style('fill', '#135')
-                .attr('x', scaleIncome(netIncome + cleanedAllowance))
-                .attr('width', Math.max(0, scaleIncome(cleanedExpense)));
+                .attr('y', scaleIncome(netIncome + cleanedAllowance))
+                .attr('height', Math.max(0, scaleIncome(cleanedExpense)));
 
             svg.select('rect.allowance')
-                .attr('height', barHeight)
+                .attr('width', barWidth)
                 .transition().duration(transitionDuration)
                 .style('fill', '#147')
-                .attr('x', scaleIncome(netIncome))
-                .attr('width', Math.max(0, scaleIncome(cleanedAllowance)));
+                .attr('y', scaleIncome(netIncome))
+                .attr('height', Math.max(0, scaleIncome(cleanedAllowance)));
+
+            svg.select('text.net-income-text')
+                .text(`เงินได้สุทธิ`)// ${numberWithCommas(netIncome)} บาท`)
+                .attr('x', barWidth / 2)
+                .attr('y', -scaleIncome(netIncome / 2));
+
+            svg.select('text.net-expense-text')
+                .text(`ค่าลดหย่อน`)// ${numberWithCommas(allowance)} บาท`)
+                .attr('x', barWidth / 2)
+                .attr('y', -scaleIncome(netIncome + cleanedAllowance / 2));
+
+            svg.select('text.net-allowance-text')
+                .text(`ค่าใช้จ่าย`)// ${numberWithCommas(expense)} บาท`)
+                .attr('x', barWidth / 2)
+                .attr('y', -scaleIncome(income - expense / 2));
+
+
 
             const bracketLineGroups = svg.select('g.tax-lines')
                 .selectAll('g')
@@ -97,44 +188,49 @@ export const D3Component = ({
 
             bracketLineGroups.merge(bracketLineGroupsEnter)
                 .select('rect')
-                .attr('y', d => scaleTaxRate(d.taxRate))
-                .attr('height', d => scaleTaxRate(1 - d.taxRate))
+                .attr('x', 0)   // d => scaleTaxRate(d.taxRate))
+                .attr('width', d => scaleTaxRate(d.taxRate))
                 .style('fill-opacity', 0)
                 .style('stroke', '#f80')
+                .style("stroke-dasharray", "3, 3")
                 .transition().duration(transitionDuration)
-                .attr('x', d => scaleIncome(d.minNetIncome))
-                .attr('width', d => scaleIncome(d.maxNetIncome - d.minNetIncome))
+                .attr('y', d => scaleIncome(d.minNetIncome))
+                .attr('height', d => scaleIncome(d.maxNetIncome - d.minNetIncome))
 
             bracketLineGroups.merge(bracketLineGroupsEnter)
                 .select('line')
-                .attr('y1', 0)
-                .attr('y2', barHeight)
+                .attr('x1', 0)
+                .attr('x2', barWidth)
                 .style('stroke', '#f80')
                 .style("stroke-dasharray", "3, 3")
                 .transition().duration(transitionDuration)
                 // .style('stroke-opacity', '0.4')
-                .attr('x1', d => scaleIncome(d.maxNetIncome))
-                .attr('x2', d => scaleIncome(d.maxNetIncome))
+                .attr('y1', d => scaleIncome(d.maxNetIncome))
+                .attr('y2', d => scaleIncome(d.maxNetIncome))
 
             bracketLineGroups.merge(bracketLineGroupsEnter)
                 .select('text.bracket-rate-text')
-                .style('fill', '#f80')
-                .attr('y', -30)
+                .style('fill', '#fff')
+                .style('font-size', '0.75rem')
+                .style('text-anchor', 'end')
+                .attr('x', barWidth - 5)
                 .transition().duration(transitionDuration)
-                .text(d => `อัตราภาษี ${d.taxRate * 100}%`)
-                .style('fill-opacity', d => scaleIncome(d.maxNetIncome - d.minNetIncome) > 150 ? 1 : 0)
-                .attr('x', d => scaleIncome(d.minNetIncome))
+                .text(d => `เริ่มคิดที่ ${numberWithCommas(d.minNetIncome)} บาท`)
+                .style('fill-opacity', (d, i) => i != 0 && scaleIncome(d.maxNetIncome - d.minNetIncome) > 20 ? 1 : 0)
+                .attr('y', d => -scaleIncome(d.minNetIncome) - 5)
 
             bracketLineGroups.merge(bracketLineGroupsEnter)
                 .select('text.bracket-min-text')
-                .style('fill', '#f80')
-                .attr('y', -10)
+                .style('fill', '#fff')
+                .style('font-size', '0.75rem')
+                .attr('x', d => scaleTaxRate(d.taxRate) + 5)
                 .transition().duration(transitionDuration)
-                .text(d => scaleIncome(d.maxNetIncome - d.minNetIncome) > 150
-                    ? `เริ่มคิดที่ ${numberWithCommas(d.minNetIncome)} บาท`
-                    : `${d.taxRate * 100}%`)
-                .style('fill-opacity', d => scaleIncome(d.maxNetIncome - d.minNetIncome) > 50 ? 1 : 0)
-                .attr('x', d => scaleIncome(d.minNetIncome))
+                .text(d => `ภาษี ${d.taxRate * 100}%`)
+                // .text(d => scaleIncome(d.maxNetIncome - d.minNetIncome) > 150
+                //     ? `เริ่มคิดที่ ${numberWithCommas(d.minNetIncome)} บาท`
+                //     : `${d.taxRate * 100}%`)
+                .style('fill-opacity', (d, i) => i != 0 && scaleIncome(d.maxNetIncome - d.minNetIncome) > 20 ? 1 : 0)
+                .attr('y', d => -scaleIncome(d.minNetIncome) - 5)
 
 
 
@@ -149,11 +245,11 @@ export const D3Component = ({
                 .style('fill-opacity', 1)
 
             taxParts.merge(taxPartsEnter)
-                .attr('y', d => scaleTaxRate(d.taxRate))
-                .attr('height', d => barHeight * d.taxRate)
+                .attr('x', 0)   // d => scaleTaxRate(d.taxRate))
+                .attr('width', d => scaleTaxRate(d.taxRate))
                 .transition().duration(transitionDuration)
-                .attr('x', d => scaleIncome(d.minNetIncome))
-                .attr('width', d => {
+                .attr('y', d => scaleIncome(d.minNetIncome))
+                .attr('height', d => {
                     if (netIncome > d.minNetIncome) {
                         if (netIncome < d.maxNetIncome)
                             return scaleIncome(netIncome - d.minNetIncome)
@@ -166,8 +262,9 @@ export const D3Component = ({
 
             console.log(isPullTax);
             svg.select('g.tax-parts')
-                .transition()
-                .attr('transform', isPullTax ? `translate(0, ${padding + 20})` : 'translate(0, 0)');
+                .transition(transitionDuration)
+                .attr('transform', isPullTax ? `translate(${-50}, 0)` : 'translate(0, 0)')
+                .style('opacity', isPullTax ? 0 : 1);
 
 
             const taxCamouflageParts = svg.select('g.tax-camouflage')
@@ -182,11 +279,11 @@ export const D3Component = ({
                 .style('fill-opacity', 1)
 
             taxCamouflageParts.merge(taxCamouflagePartsEnter)
-                .attr('y', d => scaleTaxRate(d.taxRate))
-                .attr('height', d => scaleTaxRate(1 - d.taxRate))
+                .attr('x', 0)   // d => scaleTaxRate(d.taxRate))
+                .attr('width', d => scaleTaxRate(d.taxRate))
                 .transition().duration(transitionDuration)
-                .attr('x', d => scaleIncome(d.minNetIncome))
-                .attr('width', d => {
+                .attr('y', d => scaleIncome(d.minNetIncome))
+                .attr('height', d => {
                     if (netIncome > d.minNetIncome) {
                         if (netIncome < d.maxNetIncome)
                             return scaleIncome(netIncome - d.minNetIncome)
@@ -201,9 +298,9 @@ export const D3Component = ({
             // AXES
 
             d3.select("g.axis-income-bottom")
-                .attr("transform", `translate(0, ${barHeight})`)
+                .attr("transform", `translate(0, 0)`)
                 .transition().duration(transitionDuration)
-                .call(axisIncomeBottom);
+                .call(axisIncomeLeft);
 
             // d3.select("g.axis-income-top")
             //     .attr("transform", `translate(0, -10)`)
@@ -252,19 +349,19 @@ export const D3Component = ({
             // boxGroups.exit().remove();
         }
     }, [
+        width, height,
         d3Container,
         isPullTax,
-        barHeight,
+        barWidth,
         scaleIncome,
         netIncome,
         income,
         expense,
         allowance,
         taxBrackets,
-        height,
         padding,
-        axisIncomeTop,
-        axisIncomeBottom,
+        axisIncomeLeft,
+        axisIncomeRight,
         axisTaxRate,
         cleanedAllowance,
         cleanedExpense,
@@ -272,25 +369,39 @@ export const D3Component = ({
     ]);
 
     return (<>
-        <svg
-            className="d3-component"
-            width={width}
-            height={height}
-            ref={d3Container}
-        >
-            <g className="container">
-                <rect className="net-income" />
-                <rect className="expense" />
-                <rect className="allowance" />
-                <g className="tax-lines" />
-                <g className="tax-camouflage" />
-                <g className="axis-income-top" />
-                <g className="axis-income-bottom" />
-                <g className="axis-tax-rate" />
-                <g className="tax-parts" />
-            </g>
-        </svg>
-        <p>{netIncome}</p>
-        <button onClick={() => setPullTax(!isPullTax)}>Tax</button>
+        <SvgContainer ref={observe}>
+            <Fade />
+            <CartesianSvg
+                id="chart"
+                className="d3-component"
+                width={width}
+                height={height}
+                ref={d3Container}
+            // viewBox={`0 0 ${width} ${height}`}
+            >
+                <g transform={`translate(0, ${height}) scale(1, -1)`}>
+                    <g className="container">
+                        <rect className="net-income" />
+                        <rect className="expense" />
+                        <rect className="allowance" />
+                        <text className="net-income-text" style={{
+                            fill: '#fff', fontSize: '0.75rem', alignmentBaseline: 'middle', textAnchor: 'middle', fillOpacity: 0.4
+                        }} />
+                        <text className="net-expense-text" style={{
+                            fill: '#fff', fontSize: '0.75rem', alignmentBaseline: 'middle', textAnchor: 'middle', fillOpacity: 0.4
+                        }} />
+                        <text className="net-allowance-text" style={{
+                            fill: '#fff', fontSize: '0.75rem', alignmentBaseline: 'middle', textAnchor: 'middle', fillOpacity: 0.4
+                        }} />
+                        <g className="tax-lines" />
+                        <g className="tax-camouflage" />
+                        <g className="axis-income-top" />
+                        <g className="axis-income-bottom" />
+                        <g className="axis-tax-rate" />
+                        <g className="tax-parts" />
+                    </g>
+                </g>
+            </CartesianSvg>
+        </SvgContainer>
     </>);
 }
