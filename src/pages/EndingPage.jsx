1c66061d2d8a16f47ac656fb317ee0e82bb0c1b8
Tae Prasongpongchai <taepras@gmail.com>
2021-06-22 23:44:28 +0700

    update based on feedback from Thai Gov Design team

--- src/pages/EndingPage.jsx
+++ src/pages/EndingPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { transparentize } from 'polished';
 import { theme } from '../theme';
 import Button from '../components/Button';
 import Step from '../components/Step';
 import Pill from '../components/Pill';
 
-const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeStepsCount }) => (
+const EndBox = styled.div`
+  padding: 16px 32px;
+  max-width: 100%;
+  background-color: ${transparentize(0.92, theme.colors.taxText)};
+  border-radius: 8px;
+  margin-bottom: 2rem;
+
+  p {
+    margin: 0;
+  }
+`;
+
+const EndingPage = ({
+  currentNarrativeStep, setCurrentNarrativeStep, narrativeStepsCount, tax, income,
+}) => (
   <Step
     status={Math.sign(narrativeStepsCount - currentNarrativeStep)}
     style={{
-      gap: '10px',
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: theme.colors.bg,
@@ -19,18 +34,29 @@ const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeSt
       textAlign: 'center',
     }}
   >
+    <EndBox>
+      <p>
+        ดังนั้น คุณจะต้องเสียภาษีประมาณ
+      </p>
+      <p style={{ margin: `0.5rem ${0}`, color: theme.colors.taxText }}>
+        <h1 style={{ display: 'inline', margin: 0 }}>{Math.round(tax).toLocaleString()}</h1>
+        {' '}
+        <b>บาท</b>
+        <br />
+      </p>
+      <p>
+        หรือคิดเป็น
+        {' '}
+        <b style={{ color: theme.colors.taxText }}>
+          {((tax / income) * 100).toFixed(1)}
+          %
+        </b>
+        {' '}
+        ของเงินได้ทั้งหมด
+      </p>
+    </EndBox>
     <p>
-      และนี่คือการคำนวณ
-      <br />
-      <Pill color="taxText">ภาษีเงินได้บุคคลธรรมดา</Pill>
-      เบื้องต้น
-      <br />
-      {/* <small style={{ color: theme.colors.textSecondary }}>(อ้างอิงข้อมูลปีภาษี 2563)</small>
-    </p>
-    <p> */}
-      หากสนใจเรื่องการคำนวณภาษี
-      <br />
-      อย่างละเอียด สามารถอ่านได้จาก
+      ศึกษาข้อมูลเรื่องภาษีเพิ่มเติมได้จาก
       <br />
       {' '}
       <a href="https://www.rd.go.th/548.html" target="_blank" rel="noreferrer">กรมสรรพากร</a>
@@ -40,20 +66,18 @@ const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeSt
       <a href="https://www.google.com/search?q=%E0%B8%84%E0%B8%B3%E0%B8%99%E0%B8%A7%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B5" title="Google: 'คำนวณภาษี'" target="_blank" rel="noreferrer">แหล่งข้อมูลอื่นๆ</a>
     </p>
     <p>
-      แต่ถ้าชอบ visualization นี้
-      <br />
-      ก็อย่าลืมแชร์ต่อให้เพื่อนได้เลย!
+      แต่ถ้าชอบ visualization นี้ก็แชร์ต่อได้เลย!
     </p>
-    <div style={{ display: 'flex', gap: '10px' }}>
+    <div style={{ display: 'flex', gap: '10px', marginBottom: '1rem' }}>
       <div
         className="fb-share-button"
-        data-href="https://taepras.github.io/taxvisualizer/"
+        data-href="https://taepras.github.io/howtotax/"
         data-layout="button"
         data-size="large"
       >
         <a
           target="_blank"
-          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftaepras.github.io%2Ftaxvisualizer%2F&amp;src=sdkpreparse"
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftaepras.github.io%2Fhowtotax%2F&amp;src=sdkpreparse"
           className="fb-xfbml-parse-ignore"
           rel="noreferrer"
         >
@@ -64,7 +88,7 @@ const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeSt
         href="https://twitter.com/share?ref_src=twsrc%5Etfw"
         className="twitter-share-button"
         data-size="large"
-        data-url="https://taepras.github.io/taxvisualizer/"
+        data-url="https://taepras.github.io/howtotax/"
         data-via="taepras"
         data-dnt="true"
         data-show-count="false"
@@ -76,7 +100,7 @@ const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeSt
         data-lang="en"
         data-type="share-a"
         data-ver="3"
-        data-url="https://taepras.github.io/taxvisualizer/"
+        data-url="https://taepras.github.io/howtotax/"
         data-color="default"
         data-size="large"
         data-count="false"
@@ -111,20 +135,35 @@ const EndingPage = ({ currentNarrativeStep, setCurrentNarrativeStep, narrativeSt
       </Button>
     </div>
 
-    <p style={{ marginTop: '30px', color: theme.colors.textSecondary, fontSize: '0.875rem' }}>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: '30px',
+      color: theme.colors.textSecondary,
+      fontSize: '0.875rem',
+    }}
+    >
       <a
         href="https://taepras.com"
-        style={{ color: theme.colors.textSecondary }}
+        style={{ color: theme.colors.textSecondary, marginRight: 16 }}
         target="_blank"
         rel="noreferrer"
       >
         <img src={`${process.env.PUBLIC_URL}/logo_initials.svg`} alt="tp_" style={{ width: 64 }} />
-        <br />
-        taepras.com
       </a>
-      <br />
-      ธนวิชญ์ ประสงค์พงษ์ชัย
-    </p>
+      <div style={{ textAlign: 'left' }}>
+        <a
+          href="https://taepras.com"
+          style={{ color: theme.colors.textSecondary }}
+          target="_blank"
+          rel="noreferrer"
+        >
+          taepras.com
+        </a>
+        <br />
+        ธนวิชญ์ ประสงค์พงษ์ชัย
+      </div>
+    </div>
   </Step>
 );
 
